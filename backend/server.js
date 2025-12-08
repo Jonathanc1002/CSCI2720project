46773b0ec8f3fc2efbc3d106886b614f9c471b59
SHAWN_YEO\phoen <seunghyuny01@gmail.com>
2025-12-08 14:00:27 +0800

    add:backend structure

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+const express = require("express"); 
+const app = express(); 
+app.use(express.json()); 
+app.get("/",(req,res)=>res.send("API ok")); 
+app.listen(3000,()=>console.log("Server running"));
